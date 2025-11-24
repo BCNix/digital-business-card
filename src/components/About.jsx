f11f2a684e490959de00da81de939313e5665c0a
BCNix <ilybevanz@gmail.com>
2025-11-23 19:48:22 -0800

    Add style for About, Interests and Footer section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ export default function About(){
     return (
         <section className="about">
             <h2>About</h2>
-            <p>I’m an aspiring Front-End Developer with a strong foundation in HTML, CSS, and JavaScript, currently advancing my skills through Scrimba’s Front-End Development track. With over five years of technical support experience, I bring strong problem-solving abilities, attention to detail, and a passion for creating clean, user-friendly interfaces. I’m focused on building real-world projects and growing into a professional developer role.</p>
+            <p>I’m an aspiring Front-End Developer with a strong foundation in HTML, CSS, and JavaScript, currently advancing my skills through Scrimba’s Front-End Development track. I’m focused on building real-world projects and growing into a professional developer role.</p>
         </section>
     )
 }
--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -1,6 +1,6 @@
 export default function Interest(){
     return (
-        <section className="interest">
+        <section className="interests">
             <h2>Interests</h2>
             <p>Front-end development. Building small web projects. Technology, gadgets, and learning new tools. Sports and fitness. Cooking and exploring new recipes.</p>
         </section>
